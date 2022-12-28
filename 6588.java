b4f7808a8fc56ee0d60f8ded67ef1525876145e3
maark1106 <121409074+maark1106@users.noreply.github.com>
2022-12-28 16:15:35 +0900

    Update 6588.java

--- 6588.java
+++ 6588.java
@@ -4,27 +4,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class main {
+public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		int[] arr1 = new int[1000001];
+		boolean[] arr1 = new boolean[1000001];
 		
 		for(int i  = 2; i*i <= 1000000;i++ ) {
-			if(arr1[i] == 0) {
+			if(!arr1[i]) {
 				for(int j = 2; i*j <= 1000000;j++) {
-					arr1[i*j] = 1;
+					arr1[i*j] = true;
 				}
 			}
 		}
-		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		while(true) {
-			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			
 			int num = Integer.parseInt(bf.readLine());
 			if(num == 0)
 				break;
 			boolean flag = true;
 			
 			for(int i = 3 ; i < num;i++) {
-				if(arr1[i] == 0 && arr1[num - i] == 0) {
+				if(!arr1[i] && !arr1[num - i]) {
 					System.out.println(num + " = " + i + " + " + (num - i));
 					flag = false;
 					break;
