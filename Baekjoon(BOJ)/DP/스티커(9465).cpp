b0b0d97ed2f586cfb1220e242fb46b7cddbce796
maark1106 <121409074+maark1106@users.noreply.github.com>
2023-11-25 11:47:10 +0900

    Add files via upload

--- Baekjoon(BOJ)/DP/스티커(9465).cpp
+++ Baekjoon(BOJ)/DP/스티커(9465).cpp
@@ -24,11 +24,10 @@ int main() {
 		dp[1][0] = arr[1][0];
 		dp[1][1] = arr[1][1];				
 
-		for (int i = 2; i <= N; i++) {
-			int pprevMax = max(dp[i - 2][0], dp[i - 2][1]);
+		for (int i = 2; i <= N; i++) {			
 
-			dp[i][0] = max(dp[i - 1][1], pprevMax) + arr[i][0];
-			dp[i][1] = max(dp[i - 1][0], pprevMax) + arr[i][1];
+			dp[i][0] = max(dp[i - 1][1], dp[i - 2][1]) + arr[i][0];
+			dp[i][1] = max(dp[i - 1][0], dp[i - 2][0]) + arr[i][1];
 		}		
 
 		cout << max(dp[N][0], dp[N][1]) << "\n";
