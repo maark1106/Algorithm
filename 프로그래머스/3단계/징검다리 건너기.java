0f2b1fa56612c338d7cfeaba918de22a64c6bbad
maark1106 <121409074+maark1106@users.noreply.github.com>
2024-09-12 01:07:08 +0900

    Update 징검다리 건너기.java

--- 프로그래머스/3단계/징검다리 건너기.java
+++ 프로그래머스/3단계/징검다리 건너기.java
@@ -1,5 +1,52 @@
 import java.util.*;
 
+// 이분탐색 사용 
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        
+        int maxValue = 0;
+        for(int stone : stones){
+            if(maxValue < stone){
+                maxValue = stone;
+            }
+        }
+        
+        int left = 1;
+        int right = maxValue;
+        
+        while(left <= right){
+            int mid = (left + right) / 2;
+            
+            int cnt = 0;
+            boolean flag = true;
+            for(int i = 0 ; i < stones.length; i++){
+                if(stones[i] < mid){
+                    cnt++;
+                }else{
+                    cnt = 0;
+                }
+                
+                if(cnt == k){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        
+        answer = right;
+        
+        return answer;
+    }
+}
+
+//Deque 사용
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = Integer.MAX_VALUE;
